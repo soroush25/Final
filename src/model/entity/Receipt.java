38c7e3fa5c08950542f73ea259d32bef1d8167cc
soroush <soroosh1480@gmail.com>
2024-06-15 12:21:52 +0330

    Commit

---
+++ src/model/entity/Receipt.java
@@ -0,0 +1,29 @@
+package src.model.entity;
+
+import com.google.gson.Gson;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import src.model.entity.enums.TransactionTypes;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+public class Receipt {
+    private Transaction id;
+    private Transaction amount;
+    private Transaction transactionDateTime;
+    private TransactionTypes transactionType;
+    private Account sourceAccount;
+    private Account destinationAccount;
+    private Customer firstName;
+    private Customer lastName;
+
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+}
