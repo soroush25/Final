05ee656d2f09784663d9e2c061fd423091a942f1
soroush <soroosh1480@gmail.com>
2024-06-21 16:13:08 +0330

    Commit

--- src/controller/AuthenticationController.java
+++ src/controller/AuthenticationController.java
@@ -36,7 +36,6 @@ public class AuthenticationController implements Initializable {
 
         loginBtn.setOnAction(event -> {
             try {
-                AppData.admin = AdminBl.getAdminBl().findByUsernameAndPassword(usernameField.getText(), passwordField.getText());
                 AppData.customer = CustomerBl.getCustomerBl().findByUsernameAndPassword(usernameField.getText(), passwordField.getText());
                 if (AppData.customer != null) {
                     Stage stage = new Stage();
@@ -47,15 +46,18 @@ public class AuthenticationController implements Initializable {
                     stage.show();
                     loginBtn.getScene().getWindow().hide();
                     System.out.println(AppData.customer);
-                } else if (AppData.admin != null) {
-                    Stage stage = new Stage();
-                    Scene scene = new Scene(
-                            FXMLLoader.load(WindowsManager.class.getResource("../view/Admin.fxml"))
-                    );
-                    stage.setScene(scene);
-                    stage.show();
-                    loginBtn.getScene().getWindow().hide();
-                    System.out.println(AppData.admin);
+                } else {
+                    AppData.admin = AdminBl.getAdminBl().findByUsernameAndPassword(usernameField.getText(), passwordField.getText());
+                    if (AppData.admin != null) {
+                        Stage stage = new Stage();
+                        Scene scene = new Scene(
+                                FXMLLoader.load(WindowsManager.class.getResource("../view/Admin.fxml"))
+                        );
+                        stage.setScene(scene);
+                        stage.show();
+                        loginBtn.getScene().getWindow().hide();
+                        System.out.println(AppData.admin);
+                    }
                 }
             } catch (Exception e) {
                 Alert alert = new Alert(Alert.AlertType.ERROR, "Error: \n" + e.getMessage());
