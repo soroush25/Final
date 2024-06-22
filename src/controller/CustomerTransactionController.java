ca7ff56daf3081029ee9b86e67719478569e6f89
soroush <soroosh1480@gmail.com>
2024-06-22 18:11:09 +0330

    Commit

--- src/controller/CustomerTransactionController.java
+++ src/controller/CustomerTransactionController.java
@@ -3,15 +3,19 @@ package src.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import lombok.extern.log4j.Log4j;
 import src.model.bl.AccountBl;
 import src.model.bl.TransactionBl;
 import src.model.entity.AppData;
 import src.model.entity.Transaction;
 import src.model.entity.enums.TransactionTypes;
+import src.view.WindowsManager;
 
 import java.net.URL;
 import java.sql.Timestamp;
@@ -25,7 +29,7 @@ public class CustomerTransactionController implements Initializable {
     private TextField amountField, accountField;
 
     @FXML
-    private Button customerTransferBtn, customerWithdrawalBtn, customerDepositBtn;
+    private Button customerTransferBtn, customerWithdrawalBtn, customerDepositBtn, customerReceiptBtn;
 
     @FXML
     private TableView<Transaction> customerTable;
@@ -110,6 +114,21 @@ public class CustomerTransactionController implements Initializable {
             } catch (Exception e) {
                 Alert alert = new Alert(Alert.AlertType.ERROR, "Error!\n" + e.getMessage());
                 alert.show();
+            }
+        });
+
+        customerReceiptBtn.setOnAction(event -> {
+            try {
+                Stage stage = new Stage();
+                Scene scene = new Scene(
+                        FXMLLoader.load(WindowsManager.class.getResource("../view/ReceiptPanel.fxml"))
+                );
+                stage.setScene(scene);
+                stage.show();
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Error: \n" + e.getMessage());
+                alert.show();
+                log.error("ReceiptPanel Error: " + e.getMessage());
             }
         });
         customerTable.setOnMouseClicked((event) -> {
